6294d6415526dbc42420bfa3e2b812a36c5a453c
kadubass2025 <kadubass@hotmail.com>
2025-04-09 10:39:56 -0300

    Adicionado tipos primitivos

--- Tipos-Variáveis/src/TiposVariaveis.java
+++ Tipos-Variáveis/src/TiposVariaveis.java
@@ -1,5 +1,22 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
+
+        int idade = 10;
+        long numeroGrande = 10000;
+        double salarioDouble = 2000;
+        byte idadeByte = 10;
+        final short idadeshort = 10; // O nome final adicionado na variável deixa era constante sem poder alterar o
+                                     // seu valor
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caracter = 'M';
+        final float salarioFloat = (float) 2500.0d; /*
+                                                     * O nome "final" adicionado no inicio da
+                                                     * variável deixa era constante sem poder alterar o seu valor
+                                                     */
+
         System.out.println("Hello, World!");
+        System.out.println(idade);
+        System.out.println(salarioDouble);
     }
 }
