0fb62a6d861e77d0658691d205b62ac9205659e8
kadubass2025 <kadubass@hotmail.com>
2025-05-05 15:47:57 -0300

    Pequena alteração no arquivo sistema de tv

---
+++ sistema-smart-tv/src/PaginasLivros.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class PaginasLivros {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    // 1. Entrada dos minutos
+    System.out.println("\nDigite os minutos: ");
+    int minutos = scanner.nextInt();
+    // 2. Entrada dos dias
+    System.out.println("\nDigite os dias: ");
+    int dias = scanner.nextInt();
+    // 3. Calcular economia por dia
+    System.out.println("A econimia por dia eh: " + minutos*12);
+
+  if (condition:var(boolean)) {
+    
+  } else {
+    
+  }
+    }
+  // 4. Calcular economia total
+  // 5. Exibir resultado
+}}
